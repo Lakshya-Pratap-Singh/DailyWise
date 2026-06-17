e8d3a7be059ef148c7a27969fd63a839ec32caf7
Lakshya-Pratap-Singh <lakshyaps26@gmail.com>
2026-06-17 22:32:53 +0530

    inital commit

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <p>Current Streak: 0</p>
+
+      <p>Total Habits: 0</p>
+    </div>
+  );
+}
+
+export default Dashboard;
